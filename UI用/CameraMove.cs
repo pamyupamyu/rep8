9c734a7ae51b5e987bd31e160aea6034b1b5177b
ShinkaiTomomichi <yahharogim@gmail.com>
2017-01-25 14:18:21 +0900

    １/２５

---
+++ UI用/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    private Vector3 A_loc;
+
+    // Use this for initialization
+    void Start()
+    {
+        A_loc = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            A_loc.y += 0.2f;
+            transform.localEulerAngles = A_loc;
+        
+    }
+}
